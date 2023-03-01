1f42c84bb6cddf9e1d974f42802e2bd6961489e9
Kichikov28 <dkichikov4@gmail.com>
2023-03-01 17:50:49 +0200

    Services commit!

---
+++ Restaurant/Restaurant.Services/ItemsService.cs
@@ -0,0 +1,94 @@
+﻿using System.Reflection;
+
+namespace Restaurant.Services
+{
+    using Restaurant.Data;
+    using Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Linq;
+
+    public class ItemsService
+    {
+        private AppDbContext context;
+        public string AddItem(int id, string name, decimal price)
+        {
+            StringBuilder message = new StringBuilder();
+            bool isValid = true;
+            if (id < 0)
+            {
+                message.AppendLine($"Invalid {nameof(id)}!");
+                isValid = false;
+            }
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                message.AppendLine($"Invalid {nameof(id)}!");
+                isValid = false;
+            }
+            if (price < 0)
+            {
+                message.AppendLine($"Invalid {nameof(price)}!");
+                isValid = false;
+            }
+
+            if (isValid)
+            {
+                Item item = new Item()
+                {
+                    Id = id,
+                    Name = name,
+                    Price = price
+                };
+
+                using (context = new AppDbContext())
+                {
+                    context.Items.Add(item);
+                    context.SaveChanges();
+                    message.AppendLine($"Item {id} - {name} is added!");
+                }
+            }
+            return message.ToString().TrimEnd();
+        }
+        public Item GetItemById(int id)
+        {
+            using (context = new AppDbContext())
+            {
+                return context.Items.Find(id);
+            }
+        }
+        public string UpdateItemPrice(int id, decimal newPrice)
+        {
+            using (context = new AppDbContext())
+            {
+                Item item = context.Items.Find(id);
+                if (item == null)
+                {
+                    return $"{nameof(Item)} not found!";
+                }
+                if (newPrice < 0)
+                {
+                    return $"Invalid new Price!";
+                }
+                item.Price = newPrice;
+                context.Items.Update(item);
+                context.SaveChanges();
+                return $"{nameof(Item)} {item.Name} has new price: {newPrice}";
+            }
+        }
+        public string RemoveItemByName(string name)
+        {
+            using (context = new AppDbContext())
+            {
+                Item item = context.Items.FirstOrDefault(x => x.Name == name);
+                if (item == null)
+                {
+                    return $"{nameof(item)} not found!";
+                }
+                context.Items.Remove(item);
+                context.SaveChanges();
+                return $"{nameof(item)} {item.Name} was deleted!";
+            }
+        }
+    }
+}
---
+++ Restaurant/Restaurant.Services/RestaurantsService.cs
@@ -0,0 +1,101 @@
+﻿namespace Restaurant.Services
+{
+    using Restaurant.Data;
+    using Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Restaurant.Models.Enums;
+    using System.Linq;
+
+    public class RestaurantsService
+    {
+        private AppDbContext context;
+        public string AddRestaurant(string name, string location)
+        {
+            StringBuilder sb = new StringBuilder();
+            bool isValid = true;
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                sb.AppendLine($"Invalid {nameof(name)}!");
+                isValid = false;
+            }
+            if (string.IsNullOrWhiteSpace(location))
+            {
+                sb.AppendLine($"Invalid {nameof(location)}!");
+                isValid = false;
+            }
+            if (isValid)
+            {
+                Restaurant restaurant = new Restaurant()
+                {
+                    Name = name,
+                    Location = location
+                };
+                using (context = new AppDbContext())
+                {
+                    context.Restaurants.Add(restaurant);
+                    context.SaveChanges();
+                    sb.AppendLine($"Restaurant {name} located in {location} is added!");
+                }
+            }
+            return sb.ToString().TrimEnd();
+        }
+        public Restaurant GetRestaurantByName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Invalid Restaurant name!");
+            }
+            using (context = new AppDbContext())
+            {
+                Restaurant r = context.Restaurants.FirstOrDefault(x => x.Name == name);
+                context.SaveChanges();
+                return r;
+            }
+        }
+        public Restaurant GetRestaurantById(int id)
+        {
+            using (context = new AppDbContext())
+            {
+                return context.Restaurants.Find(id);
+            }
+        }
+        public string GetRestaurantInfo()
+        {
+            Restaurant r = null;
+            using (context = new AppDbContext())
+            {
+                r = context.Restaurants.Find(r);
+            }
+            if (r != null)
+            {
+                StringBuilder msg = new StringBuilder();
+                msg.AppendLine($"{nameof(Restaurant)} info: ");
+                msg.AppendLine($"\tId: {r.Id}");
+                msg.AppendLine($"\tName: {r.Name}");
+                msg.AppendLine($"\tLocation: {r.Location}");
+                msg.AppendLine($"\tType: {r.Type}");
+                return msg.ToString().TrimEnd();
+            }
+            else
+            {
+                return $"{nameof(Restaurant)} not found!";
+            }
+
+        }
+        public string DeleteRestaurantById(int id)
+        {
+            using (context = new AppDbContext())
+            {
+                Restaurant restaurant = context.Restaurants.Find(id);
+                if (restaurant == null) { return $"{nameof(Restaurant)} not found!"; }
+                context.Restaurants.Remove(restaurant);
+                context.SaveChanges();
+                return $"{nameof(Restaurant)} {restaurant.Name} in {restaurant.Location} was deleted!";
+            }
+        }
+
+    }
+}
