daf003dc9a941b6496a3ea7d8831dbe2c1fb2069
Aksel0123 <akselerado@gmail.com>
2023-03-12 02:16:27 +0200

    CreateOrderForm

--- Restaurant/Restaurant.FormApp/CreateOrderForm.Designer.cs
+++ Restaurant/Restaurant.FormApp/CreateOrderForm.Designer.cs
@@ -28,12 +28,212 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Create = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Order = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelPages = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.Create.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Create
+            // 
+            this.Create.Controls.Add(this.button3);
+            this.Create.Controls.Add(this.button2);
+            this.Create.Controls.Add(this.comboBox4);
+            this.Create.Controls.Add(this.label4);
+            this.Create.Controls.Add(this.comboBox3);
+            this.Create.Controls.Add(this.label3);
+            this.Create.Controls.Add(this.label2);
+            this.Create.Controls.Add(this.label1);
+            this.Create.Controls.Add(this.comboBox2);
+            this.Create.Controls.Add(this.comboBox1);
+            this.Create.Location = new System.Drawing.Point(31, 23);
+            this.Create.Name = "Create";
+            this.Create.Size = new System.Drawing.Size(360, 344);
+            this.Create.TabIndex = 0;
+            this.Create.TabStop = false;
+            this.Create.Text = "Create order";
+            this.Create.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(207, 226);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 52);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 226);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 52);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Add to the order";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(160, 35);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(151, 28);
+            this.comboBox4.TabIndex = 8;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Choose cuisine";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(162, 175);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(151, 28);
+            this.comboBox3.TabIndex = 6;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Choose item";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Choose restaurant";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Choose town";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(160, 127);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(151, 28);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(160, 78);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Order
+            // 
+            this.Order.FormattingEnabled = true;
+            this.Order.ItemHeight = 20;
+            this.Order.Location = new System.Drawing.Point(476, 43);
+            this.Order.Name = "Order";
+            this.Order.Size = new System.Drawing.Size(385, 324);
+            this.Order.TabIndex = 2;
+            this.Order.SelectedIndexChanged += new System.EventHandler(this.Order_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(476, 386);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 55);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Previous";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Next);
+            // 
+            // labelPages
+            // 
+            this.labelPages.AutoSize = true;
+            this.labelPages.Location = new System.Drawing.Point(659, 403);
+            this.labelPages.Name = "labelPages";
+            this.labelPages.Size = new System.Drawing.Size(15, 20);
+            this.labelPages.TabIndex = 6;
+            this.labelPages.Text = "-";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(727, 386);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 55);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Next";
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // CreateOrderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(909, 465);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.labelPages);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Order);
+            this.Controls.Add(this.Create);
+            this.Name = "CreateOrderForm";
             this.Text = "CreateOrderForm";
+            this.Load += new System.EventHandler(this.CreateOrderForm_Load);
+            this.Create.ResumeLayout(false);
+            this.Create.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox Create;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox Order;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelPages;
+        private System.Windows.Forms.Button button4;
     }
 }
--- Restaurant/Restaurant.FormApp/CreateOrderForm.cs
+++ Restaurant/Restaurant.FormApp/CreateOrderForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Restaurant.Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,9 +11,82 @@ namespace Restaurant.FormApp
 {
     public partial class CreateOrderForm : Form
     {
+        private ItemsService service1;
+        private RestaurantsService service2;
+        private int currentPage = 1;
+        private int itemsPerPage = 10;
+        private int totalPages = 0;
         public CreateOrderForm()
         {
             InitializeComponent();
+        }
+
+        private void CreateOrderForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Next(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Previous(object sender, EventArgs e)
+        {
+            if (currentPage <= 1) { return; }
+            Order.Items.Clear();
+            List<string> list = service1.GetItemsBasicInfo(--currentPage, itemsPerPage);
+            list.ForEach(p => Order.Items.Add(p));
+            labelPages.Text = $"{currentPage} / {totalPages}";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Order_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            if (currentPage >= totalPages) { return; }
+            Order.Items.Clear();
+            List<string> list = service1.GetItemsBasicInfo(++currentPage, itemsPerPage);
+            list.ForEach(p => Order.Items.Add(p));
+            labelPages.Text = $"{currentPage} / {totalPages}";
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
